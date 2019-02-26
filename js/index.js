42c8ffa69d052f3a2377e234276d553d68b2958c
SaviosSAP <47894876+SaviosSAP@users.noreply.github.com>
2019-02-26 18:30:24 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -5,25 +5,4 @@ $input.blur(function (e) {
 });
 
 
-$("#registrar").click(function() {
-  
-  var nombre = $("#username").val();
-  var email =  $("#email").val();
-  var empresa = $("#empresa").val();
-  var ocupacion = $("#ocupacion").val();
-  var telefono = $("#telefono").val();
-  
-  if ($("#username").val("") || $("#email").val("") || 
-  $("#empresa").val("") || $("#ocupacion").val("") || $("#telefono").val("")){
-  alert( "Por favor diligenciar todos los campos" );
-  
-  $('#form1').attr('onsubmit','return false;');
 
-
-    $("#username").val(nombre);
-    $("#email").val(email);
-    $("#empresa").val(empresa);
-    $("#ocupacion").val(ocupacion);
-    $("#telefono").val(telefono);
-  }
-});
