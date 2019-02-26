ec6b283ee04bde8ee83a6407cb21f9563e3ca80f
SaviosSAP <47894876+SaviosSAP@users.noreply.github.com>
2019-02-26 18:18:41 -0500

    registrar

--- js/index.js
+++ js/index.js
@@ -3,3 +3,25 @@ var $input = $('.form-fieldset > input');
 $input.blur(function (e) {
   $(this).toggleClass('filled', !!$(this).val());
 });
+
+$("#registrar").click(function() {
+  
+  var nombre = $("#username").val();
+  var email =  $("#email").val();
+  var empresa = $("#empresa").val();
+  var ocupacion = $("#ocupacion").val();
+  var telefono = $("#telefono").val();
+  
+  if ($("#username").val("") || $("#email").val("") || 
+  $("#empresa").val("") || $("#ocupacion").val("") || $("#telefono").val("")){
+  alert( "Por favor diligenciar todos los campos" );
+  
+  $('#form1').attr('onsubmit','return false;');
+
+
+    $("#username").val(nombre);
+    $("#email").val(email);
+    $("#empresa").val(empresa);
+    $("#ocupacion").val(ocupacion);
+    $("#telefono").val(telefono);
+  }
